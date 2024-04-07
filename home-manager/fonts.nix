6d0b15d1f9f1edac0ca6b9e3f72841251b657635
0xmycf <mycf.mycf.mycf@gmail.com>
2024-04-07 18:46:39 +0200

    99   2024-04-07 18:45:47   (current)

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,7 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  kp-font = pkgs.fetchzip {
+    url = "https://mirrors.ctan.org/fonts/kpfonts-otf.zip";
+    hash = "sha256-NneP21aeCqvKJDStt7Q9zaBCHVQtrjUlsasTBRp3p38=";
+  };
+in {
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
     fira-code
+    atkinson-hyperlegible
+    cascadia-code
+    kp-font
   ];
 }
