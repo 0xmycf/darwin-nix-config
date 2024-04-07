30aa02c3c2dca31c988a935f79e013945e634d2a
0xmycf <mycf.mycf.mycf@gmail.com>
2024-04-07 19:04:15 +0200

    100   2024-04-07 19:03:23   (current)

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -3,9 +3,26 @@
   lib,
   ...
 }: let
-  kp-font = pkgs.fetchzip {
+  kp-font-data = pkgs.fetchzip {
     url = "https://mirrors.ctan.org/fonts/kpfonts-otf.zip";
     hash = "sha256-NneP21aeCqvKJDStt7Q9zaBCHVQtrjUlsasTBRp3p38=";
+  };
+  kp-font = pkgs.stdenvNoCC.mkDerivation {
+    pname = "KpFont";
+    version = "1";
+
+    src = kp-font-data;
+
+    installPhase = ''
+      mkdir -p $out/share/fonts
+      cp -R $src/fonts $out/share/fonts/opentype/
+    '';
+
+    meta = with lib; {
+      description = "kpfonts – A complete set of fonts for text and mathematics";
+      homepage = "https://ctan.org/pkg/kpfonts-otf";
+      license = licenses.ofl;
+    };
   };
 in {
   fonts.fontconfig.enable = true;
