9e0b7b1a00012aaf5fa93fd046538c3c80fd6fa5
0xmycf <mycf.mycf.mycf@gmail.com>
2024-04-15 17:37:57 +0200

    115   2024-04-15 17:35:03   (current)

--- home-manager/programs/vscode/default.nix
+++ home-manager/programs/vscode/default.nix
@@ -41,7 +41,78 @@ in {
           version = "1.7.0";
           sha256 = "sha256-mIcSZANZlj5iO2oLiJBUHn08rXVhu/9SKsRhlu/hcvI=";
         }
+
+        {
+          name = "vscode-pvs";
+          publisher = "paolomasci";
+          version = "1.0.64";
+          sha256 = "sha256-xKxIMYVhCTfCHwG/ecfn/ae3pr4Ku/37KBB4lbHWgNI=";
+        }
       ];
     # languageSnippets = snippets;
+
+    userSettings = {
+      "workbench.iconTheme" = "material-icon-theme";
+      "workbench.colorCustomizations" = {
+        "symbolIcon.keywordForeground" = "#9586CF";
+      };
+      "editor.tokenColorCustomizations" = {
+        "textMateRules" = [
+          {
+            "scope" = "constant.other.haskell";
+            "settings" = {
+              "foreground" = "#EBCB8B";
+            };
+          }
+        ];
+      };
+      "security.workspace.trust.untrustedFiles" = "open";
+      "explorer.confirmDelete" = false;
+      "editor.suggestSelection" = "first";
+      "vsintellicode.modify.editor.suggestSelection" = "automaticallyOverrodeDefaultValue";
+      "editor.fontFamily" = "Fira Code";
+      "editor.fontLigatures" = "'onum', 'ss04', 'ss08', 'ss09', 'ss03'";
+      "editor.lineHeight" = 0;
+      "editor.fontSize" = 14;
+      "editor.fontWeight" = 600;
+      "workbench.fontAliasing" = "antialiased";
+      "editor.quickSuggestions" = {
+        "comments" = "on";
+        "strings" = "on";
+        "other" = "on";
+      };
+      "[markdown]" = {
+        "editor.quickSuggestions" = {
+          "comments" = "on";
+          "strings" = "on";
+          "other" = "on";
+        };
+      };
+      "latex-utilities.countWord.format" = "\${wordsBody} Words";
+      "files.exclude" = {};
+      "explorer.confirmDragAndDrop" = false;
+      "cSpell.language" = "en,en-GB,de-DE,de";
+      "zenMode.hideLineNumbers" = false;
+      "vim.normalModeKeyBindingsNonRecursive" = [];
+      "vim.leader" = "<space>";
+      "vim.vimrc.enable" = true;
+      "editor.cursorBlinking" = "smooth";
+      "telemetry.telemetryLevel" = "off";
+      "workbench.colorTheme" = "Spacegray Ocean Dark";
+      "files.autoSave" = "onWindowChange";
+      "editor.bracketPairColorization.enabled" = false;
+      "notebook.cellFocusIndicator" = "border";
+      "window.restoreWindows" = "none";
+      "notebook.output.textLineLimit" = 300;
+      "git.openRepositoryInParentFolders" = "never";
+      "notebook.lineNumbers" = "on";
+      "update.mode" = "manual";
+      "editor.minimap.enabled" = false;
+      "extensions.experimental.affinity" = {
+        "asvetliakov.vscode-neovim" = 1;
+      };
+    };
+
+    keybindings = builtins.fromJSON (builtins.readFile ../../.config/vscode/keybindings.json);
   };
 }
