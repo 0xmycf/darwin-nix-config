c531264e7c1f6d10586183d575dbcd30d05d9c92
0xmycf <mycf.mycf.mycf@gmail.com>
2025-02-01 14:37:12 +0100

    189   2025-02-01 14:35:53   (current)

--- home-manager/programs/fish/functions/nixshell.fish
+++ home-manager/programs/fish/functions/nixshell.fish
@@ -1,9 +1,13 @@
-set -l packages ""
 for x in $argv
     set packages $packages "nixpkgs#"$x
 end
 
 # for editing the prompt
 set -x IN_NIX_SHELL 1
+
+# just to show whats in there
+set_color red
+printf "packages are: [ $packages ]\n"
+set_color normal
 
 nix shell $packages
