37571a8668b67ef38502018931a3f08de8aaf665
0xmycf <mycf.mycf.mycf@gmail.com>
2025-05-28 10:30:40 +0200

    reactivate scala metals, but not for java files

--- home-manager/.config/nvim/lua/me/lazy.lua
+++ home-manager/.config/nvim/lua/me/lazy.lua
@@ -334,31 +334,30 @@ require("lazy").setup({
   },
 
   -- scala
-  -- {
-  --   "scalameta/nvim-metals",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --   },
-  --   ft = { "scala", "sbt", "java" },
-  --   opts = function()
-  --     local metals_config = require("metals").bare_config()
-  --     metals_config.on_attach = function(client, bufnr)
-  --       require("me.lsp").on_attach(client, bufnr)
-  --       -- your on_attach function
-  --     end
-  --
-  --     return metals_config
-  --   end,
-  --   config = function(self, metals_config)
-  --     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-  --     vim.api.nvim_create_autocmd("FileType", {
-  --       pattern = self.ft,
-  --       callback = function()
-  --         require("metals").initialize_or_attach(metals_config)
-  --       end,
-  --       group = nvim_metals_group,
-  --     })
-  --   end
-  -- }
+  {
+    "scalameta/nvim-metals",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = { "scala", "sbt", --[[ "java" ]] }, -- including java here means creating a .meatals folder everyhwere where a java file is, which is kind of annoying 
+    opts = function()
+      local metals_config = require("metals").bare_config()
+      metals_config.on_attach = function(client, bufnr)
+        require("me.lsp").on_attach(client, bufnr)
+      end
+
+      return metals_config
+    end,
+    config = function(self, metals_config)
+      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = self.ft,
+        callback = function()
+          require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+      })
+    end
+  }
 
 }, lazy_opts)
